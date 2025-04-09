d1592fc2bec364db6c08b5be65292b1a83025e4c
evechen0918 <eveyes5428@gmail.com>
2025-04-09 10:23:22 +0800

    0409-1

---
+++ my-react-demo/src/App0409-1.jsx
@@ -0,0 +1,44 @@
+import img1 from "./assets/image/switzerland-1.jpeg"
+function App() {
+    const data = {
+        prod1: {
+            imgUrl: img1,
+            title: 'Card title',
+            desc: "Some quick example text to build on the card title and make up the bulk of the card's content",
+            link: 'https://www.pinterest.com/pin/125678645841216085/',
+            btnName: '圖片來源'
+        },
+        prod2: {
+            imgUrl: img1,
+            title: 'Card title',
+            desc: "Some quick example text to build on the card title and make up the bulk of the card's content",
+            link: 'https://www.pinterest.com/pin/125678645841216085/',
+            btnName: '圖片來源'
+        }
+    }
+    return (
+        <>
+            <div style={{
+                display:'flex',
+            }}>
+                    <img src={data.prod1.imgUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{data.prod1.title}</h5>
+                        <p className="card-text">{data.prod1.desc}</p>
+                        <a href={data.prod1.link} className="btn btn-primary">{data.prod1.btnName}</a>
+                    </div>
+                </div>
+
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={data.prod2.imgUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{data.prod2.title}</h5>
+                        <p className="card-text">{data.prod2.desc}</p>
+                        <a href={data.prod2.link} className="btn btn-primary">{data.prod2.btnName}</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default App
