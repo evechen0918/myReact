26bccc61c5c44b58087dcaa846522ad104da4158
Eve <eveyes5428@gmail.com>
2025-04-22 15:37:52 +0800

    0422-1

--- my-react-demo/src/main.jsx
+++ my-react-demo/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App0415/App0415-2.jsx'
+import App from './App0422製作購物表單/App0422-1.jsx'
 //import './index.css'
-import './style.css'
+//import './style.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
